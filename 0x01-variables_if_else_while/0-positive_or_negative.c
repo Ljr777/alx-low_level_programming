ab11a7a37d5c7cb1d49ee42ee2d2718f6020faf0
Ljr777 <benughegbe@example.com>
2022-12-12 00:47:48 -0800

    error corrected

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,9 +4,9 @@
 /* more headers goes there */
 
 /**
- * main - entry point
+ * main - Entry point
  * Description - Prints if random generated number is +tive, zero or -tive
- * Return : Always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
