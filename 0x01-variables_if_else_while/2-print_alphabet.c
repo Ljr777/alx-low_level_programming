b81af9699c1cec9469a8e716070b0e2345150294
Ljr777 <benughegbe@example.com>
2022-12-12 03:21:59 -0800

    task 3, try 1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdiio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar("\n");
+	return (0);
+}
