cac2c84a4fa9a54a37aea2f263925556e6e92f0a
Ljr777 <benughegbe@example.com>
2022-12-16 05:56:47 -0800

    task 4, try 1

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers 0-9, except 2 and 4
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		if (c !== 50 || c !== 52)
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+}
